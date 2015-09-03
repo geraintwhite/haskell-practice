3a48b808b0e07d5d986d4e6fa1575009ae2a3a2b
Geraint White <geraintwhite@gmail.com>
2015-09-03 11:54:10 +0100

    Improve lychrel checker

--- lychrel.hs
+++ lychrel.hs
@@ -2,8 +2,6 @@ isPalindrome :: Eq a => [a] -> Bool
 isPalindrome xs = (reverse xs) == xs
 
 lychrel :: Int -> Int
-lychrel 0 = 0
-lychrel n = f n
-  where f x
-          | isPalindrome $ show x = x
-          | otherwise = f $ (read $ reverse $ show x) + x
+lychrel x
+  | isPalindrome $ show x = x
+  | otherwise = lychrel $ (read $ reverse $ show x) + x
